5d368563e2b93cb91bd464683ef1cf3dfde90c7e
Haris Alic <haris.alic@students.fhnw.ch>
2024-09-02 11:15:47 +0200

    Add sign up page

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const bcrypt = require('bcrypt');
+const path = require('path');
+const { stat } = require('fs');
+
+let staticPath = path.join(__dirname, "public");
+
+const app = express();
+
+// middlewares
+app.use(express.static(staticPath));
+
+// routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(staticPath, "index.html"));
+});
+
+app.get("/signup", (req, res) => {
+    res.sendFile(path.join(staticPath, "signup.html"));
+});
+
+app.use((req, res) => {
+    res.sendFile(path.join(staticPath, "404.html"));
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
