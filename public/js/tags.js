ea4d964a59526b143c25fd38dd91bc0d1a62069f
hariveliki <haris.alic@students.fhnw.ch>
2024-10-09 15:15:02 +0200

    Uncomment index styling link

--- public/js/tags.js
+++ public/js/tags.js
@@ -3,22 +3,23 @@
   let tag1 = "landscape";
   let tag2 = "portrait";
   let tag3 = "abstract";
-  let tag1_products = await getProductsByTag(tag1);
-  let tag2_products = await getProductsByTag(tag2);
-  let tag3_products = await getProductsByTag(tag3);
-  if (tag1_products.length == 0 || tag1_products == undefined) {
+  let tagProducts1 = await getProductsByTag(tag1);
+  console.log(tagProducts1)
+  let tagProducts2 = await getProductsByTag(tag2);
+  let tagProducts3 = await getProductsByTag(tag3);
+  if (tagProducts1.length == 0 || tagProducts1 == undefined) {
     throw new Error("No products found");
   } else {
-    createProductSlider(tag1_products, `${tag1}-products`, tag1);
+    createProductSlider(tagProducts1, `${tag1}-products`, tag1);
   }
-  if (tag2_products.length == 0 || tag2_products == undefined) {
+  if (tagProducts2.length == 0 || tagProducts2 == undefined) {
     throw new Error("No products found");
   } else {
-    createProductSlider(tag2_products, `#${tag2}-products`, tag2);
+    createProductSlider(tagProducts2, `#${tag2}-products`, tag2);
   }
-  if (tag3_products.length == 0 || tag3_products == undefined) {
+  if (tagProducts3.length == 0 || tagProducts3 == undefined) {
     throw new Error("No products found");
   } else {
-    createProductSlider(tag3_products, `#${tag3}-products`, tag3);
+    createProductSlider(tagProducts3, `#${tag3}-products`, tag3);
   }
 })();
